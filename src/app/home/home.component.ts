52c6d0d4d2ee444f97473b5dc0de7e8bedcfae16
Paul Osuigwe {LoopCodes} <103355656+Loopcodes@users.noreply.github.com>
2023-03-25 16:35:42 +0100

    general updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,5 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  home = [
-    { 'id': 1, 'name': 'Tomatoes',  'price':200, 'image': '../../assets/tomato.webp' },
-    { 'id': 2, 'name': 'Carrots',  'price':200, 'image': '../../assets/carrot.jpg' },
-    { 'id': 3, 'name': 'Bell Peppers', 'price':200, 'image': '../../assets/sweet-pepper-colorful-sweet-bell-peppers-natural-royalty-free-image-1611355757_.jpg' },
-    { 'id': 4, 'name': 'Onions', 'price':200, 'image': '../../assets/onion.jpg' },
-  ]
+
 }
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -10,6 +10,13 @@ export class MarketplaceComponent implements OnInit {
 chart:any;
   constructor() { }
 
+  home = [
+    { 'id': 1, 'name': 'Tomatoes',  'price':200, 'image': '../../assets/tomato.webp' },
+    { 'id': 2, 'name': 'Carrots',  'price':200, 'image': '../../assets/carrot.jpg' },
+    { 'id': 3, 'name': 'Bell Peppers', 'price':200, 'image': '../../assets/sweet-pepper-colorful-sweet-bell-peppers-natural-royalty-free-image-1611355757_.jpg' },
+    { 'id': 4, 'name': 'Onions', 'price':200, 'image': '../../assets/onion.jpg' },
+  ]
+
   ngOnInit(): void{
   this.chart = document.getElementById('my_first_chart');
   Chart.register(...registerables);
